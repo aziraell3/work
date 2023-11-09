901084e7596113d99e73e8aba79a850fb9fa4715
Envyz711 <envyz711@gepplus.co.kr>
2023-11-09 18:44:16 +0900

    a

--- gmarket/git.js
+++ gmarket/git.js
@@ -158,6 +158,15 @@ function copyUlr() {
 		}
 	})
 }
+function noSpaceForm(obj) { // 공백사용못하게
+	var str_space = /\s/;  // 공백체크
+	if(str_space.exec(obj.value)) { //공백 체크
+		//alert("해당 항목에는 공백을 사용할수 없습니다.\n\n공백은 자동적으로 제거 됩니다.");
+		obj.focus();
+		obj.value = obj.value.replace(/\s| /gi,''); // 공백제거
+		return false;
+	}
+}
 $('#selectName').change(function() {
 	var $name = $(this).find('option:selected').text();
 	var $id = $(this).find('option:selected').val();
